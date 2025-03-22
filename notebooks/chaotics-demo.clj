9b73a80b892f4f53603f5b1ecc0131d348b2e85b
uprootiny <109288004+uprootiny@users.noreply.github.com>
2025-03-22 06:34:35 +0200

    Update chaotics-demo.clj

--- notebooks/chaotics-demo.clj
+++ notebooks/chaotics-demo.clj
@@ -20,3 +20,29 @@
                   :rows points})))
 
 (visualize-lorenz)
+
+
+(defn julia-set
+  [c max-iter x-min x-max y-min y-max width height]
+  (let [pixels (atom [])]
+    (doseq [x (range width)
+            y (range height)]
+      (let [zx (/ (* (- x (/ width 2)) 3.5) width))
+            zy (/ (* (- y (/ height 2)) 2.0) height))
+            [zx zy] (loop [zx zx zy zy iter 0]
+                      (if (>= iter max-iter)
+                        [zx zy]
+                        (let [zx2 (+ (* zx zx) (- (* zy zy)) (real c))
+                              zy2 (* 2 zx zy) (imag c)]
+                          (if (> (+ (* zx2 zx2) (* zy2 zy2)) 4)
+                            [zx zy]
+                            (recur zx2 zy2 (inc iter))))))]
+        (swap! pixels conj [x y])))
+    @pixels))
+
+(defn visualize-julia
+  []
+  (let [pixels (julia-set -0.8+0.156i 256 -1.5 1.5 -1.5 1.5 800 800)]
+    (clerk/html [:div
+                 (for [[x y] pixels]
+                   [:div {:style {:width "1px" :height "1px" :background-color "black" :position "absolute" :left (str x "px") :top (str y "px")}}])])))
